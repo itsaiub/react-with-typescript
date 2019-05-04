1bfff94d05da453ee6c788116392bf394b4bb920
itsaiub <itsaiub@gmail.com>
2019-05-05 01:17:04 +0600

    complete todoapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import "./App.css";
 
 type FormElement = React.FormEvent<HTMLFormElement>;
@@ -17,8 +17,18 @@ const App = (): JSX.Element => {
     addTodo(value);
     setValue("");
   };
-  const addTodo = (text: string) => {
+  const addTodo = (text: string): void => {
     const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+  const removeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos.splice(index, 1);
     setTodos(newTodos);
   };
   return (
@@ -32,6 +42,25 @@ const App = (): JSX.Element => {
         />
         <button type="submit">Add Todo</button>
       </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => {
+          return (
+            <Fragment key={index}>
+              <div
+                style={{ textDecoration: todo.complete ? "line-through" : "" }}
+              >
+                {todo.text}
+              </div>
+              <button type="button" onClick={() => completeTodo(index)}>
+                {todo.complete ? "Incomplete" : "Complete"}
+              </button>
+              <button type="button" onClick={() => removeTodo(index)}>
+                &times;
+              </button>
+            </Fragment>
+          );
+        })}
+      </section>
     </div>
   );
 };
